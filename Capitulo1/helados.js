19f15879868c60586b6eaa60cb0a8e53951ccefb
Any Omaña <iamanyyeei@gmail.com>
2023-06-04 19:44:17 -0400

    first commit

---
+++ Capitulo1/helados.js
@@ -0,0 +1,95 @@
+let moneyRoberto = prompt("Hola Roberto, ¿cuánto dinero tienes?");
+moneyRoberto = parseFloat(moneyRoberto);
+
+if (moneyRoberto >= 0.6 && moneyRoberto < 1){
+    alert("Roberto, cómprate el helado de agua");
+    alert("Tu vuelto es: " + (moneyRoberto - 0.6) + " USD"); 
+}
+else if (moneyRoberto >= 1 && moneyRoberto < 1.6){
+    alert("Roberto, cómprate el helado de crema");
+    alert("Tu vuelto es: " + (moneyRoberto - 1) + " USD"); 
+}
+else if (moneyRoberto >= 1.6 && moneyRoberto < 1.7){
+    alert("Roberto, cómprate el helado marca heladix ");
+    alert("Tu vuelto es: " + (moneyRoberto - 1.6) + " USD"); 
+}
+else if (moneyRoberto >= 1.7 && moneyRoberto < 1.8){
+    alert("Roberto, cómprate el helado marca heladovich ");
+    alert("Tu vuelto es: " + (moneyRoberto - 1.7) + " USD"); 
+}
+else if (moneyRoberto >= 1.8 && moneyRoberto < 2.9){
+    alert("Roberto, cómprate el helado marca helardo ");
+    alert("Tu vuelto es: " + (moneyRoberto - 1.8) + " USD"); 
+}
+else if (moneyRoberto >= 2.9){
+    alert("Roberto, cómprate el helado de confites o el pote de 1/4 de Kg");
+    alert("Tu vuelto es: " + (moneyRoberto - 2.9) + "USD"); 
+
+}
+else{
+    alert("Lo siento, pero no te alcanza para ninguno");
+}
+
+
+moneyPedro = prompt("Hola Pedro, ¿cuánto dinero tienes?");
+moneyPedro = parseFloat(moneyPedro);
+
+
+if (moneyPedro >= 0.6 && moneyPedro < 1){
+    alert("Pedro, cómprate el helado de agua");
+    alert("Tu vuelto es: " + (moneyPedro - 0.6) + " USD");
+}
+else if (moneyPedro >= 1 && moneyPedro < 1.6){
+    alert("Pedro, cómprate el helado de crema");
+    alert("Tu vuelto es: " + (moneyPedro - 1) + " USD"); 
+}
+else if (moneyPedro >= 1.6 && moneyPedro < 1.7){
+    alert("Pedro, cómprate el helado marca heladix ");
+    alert("Tu vuelto es: " + (moneyPedro - 1.6) + " USD"); 
+}
+else if (moneyPedro >= 1.7 && moneyPedro < 1.8){
+    alert("Pedro, cómprate el helado marca heladovich ");
+    alert("Tu vuelto es: " + (moneyPedro - 1.7) + " USD");
+}
+else if (moneyPedro >= 1.8 && moneyPedro < 2.9){
+    alert("Pedro, cómprate el helado marca helardo ");
+    alert("Tu vuelto es: " + (moneyPedro - 1.8) + " USD"); 
+}
+else if (moneyPedro >= 2.9){
+    alert("Pedro, cómprate el helado de confites o el pote de 1/4 de Kg");
+    alert("Tu vuelto es: " + (moneyPedro - 2.9) + "USD"); 
+}
+else{
+    alert("Lo siento, pero no te alcanza para ninguno");
+}
+
+moneyCofla = prompt("Hola Cofla, ¿cuánto dinero tienes?");
+moneyCofla= parseFloat(moneyCofla);
+
+if (moneyCofla >= 0.6 && moneyCofla < 1){
+    alert("Pedro, cómprate el helado de agua");
+    alert("Tu vuelto es: " + (moneyCofla - 0.6) + " USD");
+}
+else if (moneyCofla >= 1 && moneyCofla < 1.6){
+    alert("Cofla, cómprate el helado de crema");
+    alert("Tu vuelto es: " + (moneyCofla - 1) + " USD"); 
+}
+else if (moneyCofla >= 1.6 && moneyCofla < 1.7){
+    alert("Cofla, cómprate el helado marca heladix ");
+    alert("Tu vuelto es: " + (moneyCofla - 1.6) + " USD"); 
+}
+else if (moneyCofla >= 1.7 && moneyCofla < 1.8){
+    alert("Cofla, cómprate el helado marca heladovich ");
+    alert("Tu vuelto es: " + (moneyCofla - 1.7) + " USD");
+}
+else if (moneyCofla >= 1.8 && moneyCofla < 2.9){
+    alert("Cofla, cómprate el helado marca helardo ");
+    alert("Tu vuelto es: " + (moneyCofla - 1.8) + " USD"); 
+}
+else if (moneyCofla >= 2.9){
+    alert("Cofla, cómprate el helado de confites o el pote de 1/4 de Kg");
+    alert("Tu vuelto es: " + (moneyCofla - 2.9) + "USD"); 
+}
+else{
+    alert("Lo siento, pero no te alcanza para ninguno");
+}
---
+++ archivo.js
@@ -0,0 +1,11 @@
+let name1 = "Elena";
+
+if (name1 == "Any Omaña") {
+    alert("Hola, " + name1)
+}
+else if (name1 == "Omaña"){
+    alert("No sé quién eres");
+}
+else {
+    alert("Tu nombre es otro");
+}
